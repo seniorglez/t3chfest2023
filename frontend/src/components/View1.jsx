de5ab3e933b4e89ac50dd0b5245440eb951113c3
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2023-02-25 14:18:12 +0100

    Re-estructurar

--- frontend/src/components/View1.jsx
+++ frontend/src/components/View1.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import {Selector} from './selector';
 import '../style/border.css'
+import '../style/view1.css'
 
 function View1() {
 
   const [text , setText] = useState("")
   return (
     <div>
-        <p>Busca recursos</p>
-        <input type="text" value={text} onChange={ev => setText(ev.target.value)}/> 
+        <div className='searchBar'>
+            <p>Busca recursos</p>
+            <input type="text" value={text} onChange={ev => setText(ev.target.value)} className='input'/> 
+        </div>
         <Selector/>
+        <button>Buscar</button>
     </div>
   );
 }
