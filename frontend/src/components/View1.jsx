7c266ff09682f95bbbc6894ac2dcc962410c7eab
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2023-02-25 14:37:44 +0100

    a

--- frontend/src/components/View1.jsx
+++ frontend/src/components/View1.jsx
@@ -4,18 +4,24 @@ import '../style/border.css'
 import '../style/view1.css'
 
 function View1() {
+  
 
-  const [text , setText] = useState("")
-  return (
-    <div>
-        <div className='searchBar'>
-            <p>Busca recursos</p>
-            <input type="text" value={text} onChange={ev => setText(ev.target.value)} className='input'/> 
+    const [distance, setDistance] = useState(0)
+    const [text , setText] = useState("")
+    return (
+        <div>
+            <div className='searchBar'>
+                <p>Busca recursos</p>
+                <input type="text" value={text} onChange={ev => setText(ev.target.value)} className='input'/> 
+            </div>
+            <Selector/>
+            <div className='distance'>
+                <p>Distancia</p>
+                <input type="range" value={distance} onChange={ev => setDistance(ev.target.value)}/> 
+            </div>
+            <button>Buscar</button>
         </div>
-        <Selector/>
-        <button>Buscar</button>
-    </div>
-  );
+    );
 }
 
 export default View1;
--- frontend/src/components/selector.jsx
+++ frontend/src/components/selector.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../style/border.css'
+import '../style/view1.css'
 
 
 export function SubSelector(props){
@@ -9,7 +10,6 @@ export function SubSelector(props){
     if (props.value == props.row){
         subResourceList = ["c15" ,"quita Nieves" , "jamon"]
     }
-    //subResourcesList = props.subResourceList(props.select)
 
     return(
  
@@ -27,38 +27,26 @@ export function SubSelector(props){
 
 export function Selector(){
 
-    const extendList = () =>{
-        console.log('counter +1')
-    }
-
-    const [distance, setDistance] = useState(0)
     const [resource, setResource] = useState(-1)
     const [clicked , setCLicked] = useState(false)
     const click = [clicked, setCLicked]
 
     return(
-        <div className='border'>
-            <p>Recurso</p>
+        <div className='selector'>
+                <div className='button' onClick={() => setResource(0)} >
+                    <p>Inmueble</p>
+                    <SubSelector value = {resource} row = {0}/>
+                </div>
 
-            <div className='border' onClick={() => setResource(0)} >
-                <p>Inmueble</p>
-                <SubSelector value = {resource} row = {0}/>
-            </div>
+                <div className='button' onClick={() => setResource(1)} >
+                    <p>Vehiculo</p>
+                    <SubSelector value = {resource} row = {1}/>
+                </div>
 
-            <div className='border' onClick={() => setResource(1)} >
-                <p>Vehiculo</p>
-                <SubSelector value = {resource} row = {1}/>
-            </div>
-
-            <div className='border' onClick={() => setResource(2)}>
-                <p>Maquinaria</p>
-                <SubSelector value = {resource} row = {2} clicked={click}/>
-            </div>
-
-            
-
-            <p>Distancia</p>
-            <input type="range" value={distance} onChange={ev => setDistance(ev.target.value)}/> 
+                <div className='button' onClick={() => setResource(2)}>
+                    <p>Maquinaria</p>
+                    <SubSelector value = {resource} row = {2} clicked={click}/>
+                </div>
         </div>
     );
 }
